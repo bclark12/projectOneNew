a504205b96634a9a7ef8f2f5f74fc9ece9017d77
Barrett Clark <barrettclark@Barretts-MacBook-Pro.local>
2019-08-07 01:32:16 -0400

    added for loop to create letterBank

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 
 
 
-
+let createBank = function () {
 for (i=0; i < 26; i++) {
+    let alphabetLetters = document.createElement('div');
+    document.getElementById('letterBank').appendChild(alphabetLetters);
+    //alphabetLetters.className = "letter-" + i;
+    alphabetLetters.id = "letter-" + i;  
+    alphabetLetters.className = "letters"
+    alphabetLetters.innerText = alphabet[i]
+    
     console.log(alphabet[i])
 }
+}
+createBank()
