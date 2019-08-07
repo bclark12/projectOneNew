ac32d64b6bb50914bed17f578e7dc8624fe9a89a
Barrett Clark <barrettclark@Barretts-MacBook-Pro.local>
2019-08-07 12:29:11 -0400

    created randomize word functionality

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//create empty array to push individual letters tot hat array using CharCode and a "for loop"
+const alphabet = [];
+for (i = 65; i <= 90; i++) {
+    let arrayLetter = String.fromCharCode(i);
+    alphabet.push(arrayLetter);
+}
 
+//this is where the different words for the game can be entered
+let wordsArray = ['movie', 'heyguys', 'anotherword', 'lastone', 'jkonemore'];
 
+//divide the words into more managable pieces
+let lettersArray = [];
+for (i = 0; i < wordsArray.length; i++) {
+splitStringArray = wordsArray[i].split('');
+lettersArray.push(splitStringArray);
+};
+
+//Now the "lettersArray" array is an array of arrays.
+//The individual arrays inside this multi-dimensional array contain
+//  individual letters that represent each letter of the chosen word.
+let getRandomWordIndex = function () {
+    let randomWordIndex = (Math.floor(Math.random() * (lettersArray.length)));
+    return randomWordIndex
+};
+
+//Now we have a function that can return a random index number that is related to the
+//  length of the "lettersArray" array.
+//we can use this to choose a random array within that array
+let randomWordArray = lettersArray[getRandomWordIndex()];
+
+console.log(randomWordArray);
 
 let createBank = function () {
-for (i=0; i < 26; i++) {
-    let alphabetLetters = document.createElement('div');
-    document.getElementById('letterBank').appendChild(alphabetLetters);
-    //alphabetLetters.className = "letter-" + i;
-    alphabetLetters.id = "letter-" + i;  
-    alphabetLetters.className = "letters"
-    alphabetLetters.innerText = alphabet[i]
-    
-    console.log(alphabet[i])
-}
-}
-createBank()
+    for (i=0; i < alphabet.length; i++) {
+        let alphabetLetters = document.createElement('div');
+        document.getElementById('letterBank').appendChild(alphabetLetters);
+        alphabetLetters.id = "letter-" + i;  
+        alphabetLetters.className = "letters";
+        alphabetLetters.innerText = alphabet[i];
+    };
+};
+
+createBank();
